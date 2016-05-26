6f0feb36bb65bd8cf0999e4a733e763ef1f983e9
Jurnell Cockhren <jurnell@sophicware.com>
2016-05-25 18:45:54 -0700

    Initial repository test

---
+++ Delagram.Tests/DAL/DelagramRepositoryTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Delagram.DAL;
+
+namespace Delagram.Tests.DAL
+{
+    [TestClass]
+    public class DelagramRepositoryTest
+    {
+        [TestMethod]
+        public void RepoEnsureICanCreateAnInstance()
+        {
+            DelagramRepository repo = new DelagramRepository();
+            Assert.IsNotNull(repo);
+        }
+    }
+}
