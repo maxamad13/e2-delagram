9d8f50b4cb44cc5754367f855f73e0de01d7d41d
Jurnell Cockhren <jurnell@sophicware.com>
2016-05-25 18:35:42 -0700

    Added Context and empty repository

---
+++ Delagram/DAL/DelagramContext.cs
@@ -0,0 +1,15 @@
+﻿using Delagram.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Delagram.DAL
+{
+    public class DelagramContext : ApplicationDbContext
+    {
+        public virtual DbSet<Post> Posts { get; set; }
+        public virtual DbSet<Filter> Filters { get; set; }
+    }
+}
