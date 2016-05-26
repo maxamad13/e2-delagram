d9baf121da3456e844b6932784d93c3f1b97dd91
Jurnell Cockhren <jurnell@sophicware.com>
2016-05-25 18:35:14 -0700

    Post and Filter Models

---
+++ Delagram/Models/Filter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Delagram.Models
+{
+    public class Filter
+    {
+        public int FilterId { get; set; }
+        public string Name { get; set; }
+    }
+}
