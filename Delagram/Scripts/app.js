180227aad8e43ff771e062766d96913b08ad4690
Jurnell Cockhren <jurnell@sophicware.com>
2016-05-28 10:43:07 -0700

    enable angular

---
+++ Delagram/Scripts/app.js
@@ -0,0 +1,29 @@
+﻿var app = angular.module("PostApp", []);
+
+app.controller("PostController", function ($http) {
+
+    var self = this;
+
+    self.posts = [
+
+    ];
+
+    self.getPosts = function () {
+        $http.get("/api/Post")
+            .then(function (response) {
+                for (var i = 0; i < response.data.length; i++) {
+                    self.posts.push(response.data[i]);
+                }
+                
+            })
+    }
+
+    /*self.getPosts() <- returns result of calling of getPosts */
+    /*self.getPosts <- reference to function */
+    self.getPosts();
+    setInterval(self.getPosts,10000);
+
+    /* (function(){})(); */
+
+
+});
